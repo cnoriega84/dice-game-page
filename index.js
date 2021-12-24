77db163a076d44ce9d21f1a871c1fe2e716ccb79
cnoriega84 <47007451+cnoriega84@users.noreply.github.com>
2021-12-23 22:50:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ document.querySelector(".img2").setAttribute("src", randomNumber2)
 
 if(randomNumber1 > randomNumber2){
 /*use this when you just need to change the text of the tag*/
-  document.querySelector("h1").innerHTML = "Player 1 wins";
+  document.querySelector("h1").innerHTML = "Player 1 Wins";
 }else if(randomNumber2 > randomNumber1){
-  document.querySelector("h1").innerHTML = "Player 2 wins";
+  document.querySelector("h1").innerHTML = "Player 2 Wins";
 } else {
   document.querySelector("h1").innerHTML ="Draw";
 }
